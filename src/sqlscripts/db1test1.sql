89b5e5aebad2de1a9a4a3f12fe941e69898f9c40
cadafe <cadafe12@gmail.com>
2021-11-17 11:06:47 -0300

    sql scripts for tests

---
+++ src/sqlscripts/db1test1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS TableC (
+	columnc1 INT AUTO_INCREMENT,
+    columnc2 VARCHAR(30),
+    columnc4 BOOLEAN,
+    columnc7 INT,
+    CONSTRAINT pk_tableC PRIMARY KEY(columnc1)
+);
+
+CREATE TABLE IF NOT EXISTS TableX (
+	columnx INT PRIMARY KEY
+);
---
+++ src/sqlscripts/db2test2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS TableC (
+	columnc1 INT AUTO_INCREMENT,
+    columnc2 VARCHAR(30),
+    columnc4 INT,
+    columnc5 DATE,
+    CONSTRAINT pk_tableC PRIMARY KEY(columnc1)
+);
+
+CREATE TABLE IF NOT EXISTS TableFinal (
+	columnfinal FLOAT PRIMARY KEY
+);
+
+
+
